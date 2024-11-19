9e985b5f55e5af70402436678eb65e22f7f20508
leogalvan29 <contacto@paginaswebculiacan.net>
2024-11-19 00:26:51 -0600

    17 v

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from '@astrojs/tailwind';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  output: 'server',
+  adapter: vercel()
 });
